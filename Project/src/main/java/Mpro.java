fa51ea40aa009451acff606a8baf6168db527e73
user <user@DESKTOP-6FJKV3E>
2024-03-25 15:29:17 +0530

    first

---
+++ Project/src/main/java/Mpro.java
@@ -0,0 +1,13 @@
+
+public class Mpro {
+public static void show()
+{
+	System.out.println("hai");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mpro.show();
+		
+	}
+
+}
